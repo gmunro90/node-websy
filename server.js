eb51db59cc333d375216042a3c676d42d6b40fbc
Greg Munro <gregmunro@Websy-Greg.local>
2022-06-27 15:54:04 +0200

    simple routing set up

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const products = require('./products.json')
+
+const server = http.createServer((req, res) => {
+  
+  if(req.url === '/api/products' && req.method === 'GET')  {
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify(products))
+  }
+  else {
+    res.writeHead(404, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify({message: 'Not found'}))
+  }
+})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`))
